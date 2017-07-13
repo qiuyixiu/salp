c2b42c4d990cbee58f5c96e94b550850dd2fa0f3
imac <imac@imacdeiMac.local>
2017-07-13 21:37:02 +0800

    adjust aliyun11

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,6 @@ CarrierWave.configure do |config|
   config.aliyun_access_id     = ENV["aliyun_access_id"]
   config.aliyun_access_key    = ENV["aliyun_access_key"]
   config.aliyun_bucket        = ENV["aliyun_bucket"]
-  config.aliyun_host          = "https://yixiu2017.oss-cn-shenzhen.aliyuncs.com"
   config.aliyun_internal      = false
   config.aliyun_area          = "cn-shenzhen"
   config.aliyun_private_read  = false
