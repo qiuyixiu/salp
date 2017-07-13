de83989ef8cda5fcd42aa7c9604789ef9083dc01
yixiu <aken@aken.com>
2017-07-13 15:09:16 +0800

    adjust aliyun4

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.storage              = :aliyun
-  config.aliyun_access_id     = "xxxxxx"
-  config.aliyun_access_key    = "xxxxxx"
-  config.aliyun_bucket        = "xxxx"
-  config.aliyun_host          = "xxxx"
+  config.aliyun_access_id     = ENV["aliyun_access_id"]
+  config.aliyun_access_key    = ENV["aliyun_access_key"]
+  config.aliyun_bucket        = ENV["aliyun_bucket"]
+  config.aliyun_host          = ENV["aliyun_host"]
   config.aliyun_internal      = false
   config.aliyun_area          = "cn-shenzhen"
   config.aliyun_private_read  = true
