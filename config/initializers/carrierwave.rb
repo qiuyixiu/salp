c07c73c668c7fdfaa4abfd0441a3f40dee782c74
yixiu <aken@aken.com>
2017-07-13 17:38:11 +0800

    adjust aliyun7

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
   config.aliyun_access_id     = ENV["aliyun_access_id"]
   config.aliyun_access_key    = ENV["aliyun_access_key"]
   config.aliyun_bucket        = ENV["aliyun_bucket"]
-  config.aliyun_host          = ENV["aliyun_host"]
+  config.aliyun_host          = "https://oss-cn-shenzhen.aliyuncs.com"
   config.aliyun_internal      = false
   config.aliyun_area          = "cn-shenzhen"
   config.aliyun_private_read  = false
