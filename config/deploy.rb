256ded40db38e83c653705e618e4f2def5083495
baolong <baolong@baolongdeMacBook-Air.local>
2017-07-17 09:39:00 +0800

    adjust capistrano file2

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,19 +4,40 @@ lock "3.8.2"
 set :application, 'salp'
 set :repo_url, 'git@github.com:qiuyixiu/salp.git'
 
-# Default branch is :master # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# Default branch is :master
+
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+
 # Default deploy_to directory is /var/www/my_app_name
+
 set :deploy_to, '/home/apps/salp'
- # Default value for :format is :airbrussh.
- # set :format, :airbrussh
+
+# Default value for :format is :airbrussh.
+
+# set :format, :airbrussh
+
+
 # You can configure the Airbrussh format using :format_options.
+
 # These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.lo g', color: :auto, truncate: :auto
+
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+
+
 # Default value for :pty is false
+
 # set :pty, true
+
+
 # Default value for :linked_files is []
- # append :linked_files, 'config/database.yml', 'config/secrets.yml'
+
+# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
 # Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'pu blic/system'
+
+# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
